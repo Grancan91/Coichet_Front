ada6ea9dfe9f392c11a4f80f3c8727b19aba5616
Grancan91 <abl.lopper@gmail.com>
2024-03-05 16:09:41 +0000

    update

--- src/services/api_service.js
+++ src/services/api_service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "https://coichet-backend.onrender.com/api/",
     timeout: 3000,
     headers: (localStorage.authorization) ? { authorization: localStorage.authorization } : ""
 })
